7ffc1f126ec6137848c0387fdb406356e83e42d3
최은지 <cej0311240@gmail.com>
2025-12-23 14:28:49 +0900

    scss구성 맞춘  거 커밋하겠습니다

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,8 +1,20 @@
-
+import { NavLink } from "react-router-dom";
+import logoImg from "../../assets/images/header/logo.png";
 const Header = () => {
     return (
         <div className="header">
-        Header
+        <nav className="header-menu">
+            <img src={logoImg} alt="로고 이미지" />
+            <div className="menu-list">
+        <NavLink to="#">의류형</NavLink>
+        <NavLink to="#">기본형</NavLink>
+        <NavLink to="#">대형견 추천</NavLink>
+        <NavLink to="#">목줄 유형</NavLink>
+            </div>
+            <div className="">
+
+            </div>
+    </nav>
         </div>
     )
 }
--- src/components/main/BannerSection.js
+++ src/components/main/BannerSection.js
@@ -1,8 +1,9 @@
+import "./BannerSection.scss";
 
 const BannerSection = () => {
     return (
         <section id="sec-banner">
-        BannerSection
+
         </section>
     )
 }
