836f977932e6c0c7d84e2b276b73bf5e44406ddd
jaehee <jeahee3161@naver.com>
2025-12-24 03:01:42 +0900

    detail page 공동 컴포넌트 제작. (수정 필요함)

--- src/components/detail/Option.js
+++ src/components/detail/Option.js
@@ -1,8 +1,79 @@
+import { useEffect, useRef, useState } from "react";
+// import "./Option.scss";
+
+//부모 컴포넌트에서 json을 활용한 props로 정보 전달하기 
+
+/* props 목록 설명
+label : 사이즈 / 색상 
+option : s,m,l / black, yellow, pink 
+value : '사용자가 선택한 값'
+onChange : void 
+
+*/
+
+const Option = ({label, options=[], value='',onChange,}) => {
+
+  const [showOption, setShowOption] = useState(false);
+
+  const optionRef = useRef(null);
+
+  const placeholder = `(필수) 옵션을 선택해주세요(${label})`;
+
+  const handleToggle = () => {
+    setShowOption((prev) => !prev);
+  };
+
+  const handleSelect = (item) => {
+    onchange(item);
+    setShowOption(false)
+  }
+
+  //옵션 탭 바깥 클릭 시 닫힘 동작
+  useEffect(() => {
+    const handleOutClick = (e) => {
+      if(!optionRef.current) return;
+      if(!optionRef.current.contains(e.target)){
+        setShowOption(false);
+      }
+    };
+    document.addEventListener("mousedown", handleOutClick);
+    return () => document.removeEventListener("mousedown", handleOutClick);
+
+  }, []);
 
 
-const Option = () => {
   return (
-    <div>Option</div>
+    <div ref={optionRef} className="option-wrap">
+      {/* 옵션 선택 버튼 */}
+      <button 
+      type="button" 
+      // 클릭여부, 값 선택 여부로 className 변경 
+      className={`option ${showOption ? 'show' : ''} ${value ? 'isvalue' : ''}`}
+      onClick={handleToggle}>
+        <span className="option-placeholder">
+          {value ? value : placeholder}
+        </span>
+        <span className={`option${showOption ? 'up' : 'down'}`}/>
+      </button>
+      {
+        showOption && (
+          <ul className="option-list">
+            {
+              options.map((item) => {
+                <li key={`${label}-${item}`}>
+                  <button 
+                    type="button"
+                    className={`option-item${value === item ? 'isActive':''}`}
+                    onClick={() => {handleSelect(item)}}>
+                    {item}
+                  </button>
+                </li>
+              })
+            }
+          </ul>
+        )
+      }
+    </div>
   )
 }
 
--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,4 +1,10 @@
+import Option from '../components/detail/Option'
+import productData from '../assets/data/products.json';
 import './CartPage.scss'
+import { useState } from 'react';
+
+
+
 
 //cart에서 쓰일 product json 
 // product : id, prod_name, origin_price, sale_perc
@@ -6,6 +12,11 @@ import './CartPage.scss'
 // color : black, pink, yellow 
 // useState 사용해서 상태 보기 
 const CartPage = () => {
+
+
+    const [cartSize, setCartSize] = useState("");
+    const [cartColor, setCartColor] = useState("");
+
     return (
         <div id="cart-page">
             <p className='cart-h1'>장바구니</p>
@@ -24,9 +35,16 @@ const CartPage = () => {
                 <div className="cart-content">
                     <img src="" alt=""/>
                     <div className="cart-option">
-                        {/* json에서 정보 가져오기 
-                            상품명, 옵션(사이즈, 색상),할인률 
-                        */}
+                        <Option
+                            label="사이즈"
+                            options={productData.size}
+                            value={cartSize}
+                            onChange={setCartSize}/>
+                        <Option
+                            label="색상"
+                            options={productData.color}
+                            value={cartColor}
+                            onChange={setCartColor}/>
                     </div>
                 </div>
             </div>
